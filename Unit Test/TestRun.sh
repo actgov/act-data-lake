195fe29494175f0a347003a05dfd603eeea81998
Stuart Wilson <mapr@tstapp261vs.test.act.gov.au>
2018-03-13 11:11:05 +1100

    Updating TestRun.sh to correct path names to the local path i.e. "/home/" -> "./" and including pauses during execution

--- Unit Test/TestRun.sh
+++ Unit Test/TestRun.sh
@@ -11,16 +11,19 @@ clear
 
 # Test Case 1 : Hive Internal Database Creation
 echo 'Test Case 1 : Creation of HIVE Internal database and Tables'
-FILE=/home/TestScripts/Choose.properties
+FILE=./Choose.properties
 dbname=$(grep -i 'DatabaseName' $FILE  | cut -f2 -d'=')
 tablename=$(grep -i 'TableName' $FILE  | cut -f2 -d'=')
 echo "Creating an internal database - " $dbname
 echo  "and an internal Hive table - " $tablename
-hive -hiveconf DB_NAME=$dbname -hiveconf TABLE_NAME=$tablename -f /TestScripts/Query.hql
+hive -hiveconf DB_NAME=$dbname -hiveconf TABLE_NAME=$tablename -f ./Query.hql
+
+read -n1 -r -p "Press any key to continue..."
 clear
+
 echo 'CrashDB was created; Crash is the table name; View has been created; Go to Hue to perform querying operations in Hive'
 read -rsp $'Press any key to continue...to drop table, view and database\n' -n1 key
-hive -hiveconf DB_NAME=$dbname -hiveconf TABLE_NAME=$tablename -f /TestScripts/QueryPurge.hql
+hive -hiveconf DB_NAME=$dbname -hiveconf TABLE_NAME=$tablename -f ./QueryPurge.hql
 # Need validation Code here : Future development
 echo 'Database has been purged'
 echo 'Test Pass'
@@ -34,16 +37,16 @@ read -rsp $'Press any key to continue to next test case ...' -n1 key
 # Test Case 2 : Hive External Database Creation
 
 echo 'Test Case 2 : Creation of HIVE External database and Tables'
-FILE=/home/TestScripts/Choose.properties
+FILE=./Choose.properties
 extdbname=$(grep -i 'ExtDatabaseName' $FILE  | cut -f2 -d'=')
 exttablename=$(grep -i 'ExtTableName' $FILE  | cut -f2 -d'=')
 echo "Creating an external database - " $extdbname
 echo  "and an external Hive table - " $exttablename
-hive -hiveconf DB_NAME=$extdbname -hiveconf TABLE_NAME=$exttablename -f /home/TestScripts/Query_External.hql
+hive -hiveconf DB_NAME=$extdbname -hiveconf TABLE_NAME=$exttablename -f ./Query_External.hql
 #clear
 echo -e '\n External database and table creation successful; Hive view is created ; \n Perform Querying operations in Hive'
 read -rsp $'Press any key to continue...to drop \ partition n' -n1 key
-hive -hiveconf DB_NAME=$extdbname -hiveconf TABLE_NAME=$exttablename -f /TestScripts/QueryExtPurge.hql
+hive -hiveconf DB_NAME=$extdbname -hiveconf TABLE_NAME=$exttablename -f ./QueryExtPurge.hql
 echo 'One parition is dropped'
 # Need validation Code here : Future development
 echo 'Test Pass'
