2dad9ad1bb0d238b978f0ac4c32c860bbc403d7c
Stuart Wilson <mapr@tstapp261vs.test.act.gov.au>
2018-03-20 10:43:38 +1100

    GitHub test commit
     Please enter the commit message for your changes. Lines starting

--- Unit Test/MapRClusterCheck.sh
+++ Unit Test/MapRClusterCheck.sh
@@ -6,7 +6,7 @@
 #
 # Based on code by Selvaraaju Murugesan
 # ===========================================================================
-
+#dog
 
 #Some basic information about the cluster
 clear
@@ -28,20 +28,17 @@ hostname -i
 echo -e "\n"
 
 
-
-
-
-
 # Test 1 : Test whether the cluster is secure or not 
 
 searchstr="secure=true"
 file="/opt/mapr/conf/mapr-clusters.conf"
 echo "Secure Cluster :"
 if grep -q "$searchstr" $file; then
-        echo "Cluster is Secure:"
-        echo "Test 1 Pass"
+        echo "    Cluster is Secure."
+        echo "Test 1: Pass."
 else
-        echo "Test 1 Fail : Cluster is not Secure"
+        echo "    Cluster is not secure."
+        echo "Test 1: Fail."
 fi
 echo -e "\n"
 
@@ -49,10 +46,11 @@ echo -e "\n"
 val=$(maprcli dashboard info -json | grep nodesUsed)
 echo "Number of Nodes :"
 if echo "$val" | grep -q 3 ; then
-        echo "Number of Nodes is 3"
-        echo "Test 2 Pass "
+        echo "    Number of Nodes is 3."
+        echo "Test 2: Pass."
 else
-        echo "Test 2 Fail : Number of nodes is less than 3"
+        echo "    Number of nodes is less than 3."
+        echo "Test 2: Fail."
 fi
 echo -e "\n"
 
@@ -61,40 +59,45 @@ val=$(cat /proc/mounts | grep mapr)
 #echo $val
 echo "NFS Mounted :"
 if echo "$val" | grep -q "mapr" && echo "$val" | grep -q "nfs" ; then
-        echo "NFS Mount True"
-        echo "Test 3 Pass "
+        echo "    NFS Mount exists."
+        echo "Test 3: Pass."
 else
-        echo "Test 3 Fail : No NFS mount"
+        echo "    No NFS mount."
+        echo "Test 3: Fail."
+        
 fi
 echo -e "\n"
 
 # Test 4 : Cluster Audit
 # Test Case 78401
-echo "Cluster Audit Enabled ?"
+echo "Cluster Audit for 365 days :"
 val=$( maprcli audit info -json | head -15 | grep retentionDays | cut -d ':' -f2 | tr -d '"')
 #echo $val
 # Retention dayds is set to 365
 if [ "$val" -eq "365" ];then
-        echo "Cluster Audit Enabled"
-        echo "Test 4 Pass "
+        echo "    Cluster Audit enabled for 1 year."
+        echo "Test 4: Pass."
 else
-        echo "Test 4 Fail : No Audit Enabled"
+        echo "    No Audit enabled."
+        echo "Test 4: Fail."
 fi
 echo -e "\n"
 
 #Test 5 : Cluster Health
 # Test Case
 
-echo "Cluster Alatms raised ?"
+echo "Cluster Alatms raised :"
 val=$(maprcli alarm list)
 if [[ $val ]]; then
-        echo "Alarms are raised"
-        echo "Test 5 Fail"
+        echo "    Alarms are raised."
+	maprcli alarm list
+        echo "Test 5: Fail."
 else
-        echo "No Alarms raised"
-        echo "Test 5 Pass"
+        echo "    No Alarms raised."
+        echo "Test 5: Pass."
 fi
+echo -e "\n"
 
-#Test 6 : Linux version
-# Test Case
 
+
+ audit info -json | head -15 | grep retentionDays | cut -d ':' -f2 | tr -d '"'c
